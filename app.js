cfbf4aa980140bc542b3d5c9c1c32e12014cb77f
nakayama <nakayama@nkym.com>
2024-08-02 14:38:38 +0900

    local server setup

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, './')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
